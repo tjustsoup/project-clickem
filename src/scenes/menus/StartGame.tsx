93529a02b939e15f0cef1fb04deaa5788d1f38dd
Tom <tjustsoup@gmail.com>
2026-01-25 13:43:54 -0500

    StartGame Go button goes to "battlefield"

--- src/scenes/menus/StartGame.tsx
+++ src/scenes/menus/StartGame.tsx
@@ -9,6 +9,9 @@ export default function StartGame({
     <div className="center gap-8 size-full">
       <div className="font-pixelify text-5xl">Start Game</div>
       
+      <ClickButton onClick={() => setScene("Battlefield")} >
+        Go
+      </ClickButton>
       <ClickButton onClick={() => setScene("Main Menu")}>
         Back
       </ClickButton>
