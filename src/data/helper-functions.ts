13d2e1dc5ad0a3aafb85576be724f9dff3afaa96
Tom <tjustsoup@gmail.com>
2026-01-29 13:23:05 -0500

    `getRandomInt` added

--- src/data/helper-functions.ts
+++ src/data/helper-functions.ts
@@ -5,3 +5,14 @@ export function ObjectEntriesTyped<T extends object>(
     [Extract<keyof T, string>, T[Extract<keyof T, string>]]
   >;
 }
+
+/**
+ * Gets a random integer from 1 to the `max` provided.
+ * 
+ * @param max The highest number acquirable
+ * @param min Optional - Minimum number that will be return (default 0)
+ * @returns `number`
+ */
+export function getRandomInt(max: number, min?: number): number {
+  return Math.floor(Math.random() * (max + 1)) + (min ?? 0);
+}
